2217ab231b0fb6658a5451c8c8ebe7a79621fd57
Krista Bice <bubbybice@hotmail.com>
2017-10-20 22:08:50 -0600

    trying to fix flex menu

---
+++ main.js
@@ -0,0 +1,10 @@
+<<<<<<< Updated upstream
+=======
+function openNav() {
+    document.getElementById("mySidenav").style.width = "30%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+>>>>>>> Stashed changes
---
+++ nav.php
@@ -0,0 +1,17 @@
+<<<<<<< Updated upstream
+=======
+    <header>
+   <div class="logo"><img src="img/water_bottle_r_us.png" alt="logo"></div>
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+      <a href="index.php" class="active">Home</a>
+            <a href="shopping.php">Shop</a>
+            <a href="contact.php">Contact</a>
+            <a href="card.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+</div>
+
+
+<span style="font-size:20px;cursor:pointer" onclick="openNav()"> &#9776;</span>
+</header>
+>>>>>>> Stashed changes
