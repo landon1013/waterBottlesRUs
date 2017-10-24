a4765c4aefeba8e42beb331083b5d5a66d9b391c
Landon Call <landon@thecallfamily.com>
2017-10-23 19:11:37 -0600

    Add shopping page

---
+++ shopping.php
@@ -0,0 +1,61 @@
+<?php include_once('header.php')?>
+    <title>Checkout - Card</title>
+
+  </head>
+  <body >
+    <?php include_once('nav.php'); ?>
+<div id="shop" class="container">
+    <div id="main">
+      <div class="shop-container">
+        <h1 class="shop-title">Our Products</h1>
+        <div class="shop-container-inner">
+        <div class="shop-item">
+            <img class="shop-item-img" src="http://via.placeholder.com/240x290">
+            <h6 class="shop-item-title">Water Bottle</h6>
+            <p class="shop-item-price">$10.00</p>
+        </div>
+        <div class="shop-item">
+            <img class="shop-item-img" src="http://via.placeholder.com/240x290">
+            <h6 class="shop-item-title">Water Bottle</h6>
+            <p class="shop-item-price">$10.00</p>
+        </div>
+        <div class="shop-item">
+            <img class="shop-item-img" src="http://via.placeholder.com/240x290">
+            <h6 class="shop-item-title">Water Bottle</h6>
+            <p class="shop-item-price">$10.00</p>
+        </div>
+        <div class="shop-item">
+            <img class="shop-item-img" src="http://via.placeholder.com/240x290">
+            <h6 class="shop-item-title">Water Bottle</h6>
+            <p class="shop-item-price">$10.00</p>
+        </div>
+        <div class="shop-item">
+            <img class="shop-item-img" src="http://via.placeholder.com/240x290">
+            <h6 class="shop-item-title">Water Bottle</h6>
+            <p class="shop-item-price">$10.00</p>
+        </div>
+        <div class="shop-item">
+            <img class="shop-item-img" src="http://via.placeholder.com/240x290">
+            <h6 class="shop-item-title">Water Bottle</h6>
+            <p class="shop-item-price">$10.00</p>
+        </div>
+        <div class="shop-item">
+            <img class="shop-item-img" src="http://via.placeholder.com/240x290">
+            <h6 class="shop-item-title">Water Bottle</h6>
+            <p class="shop-item-price">$10.00</p>
+        </div>
+        <div class="shop-item">
+            <img class="shop-item-img" src="http://via.placeholder.com/240x290">
+            <h6 class="shop-item-title">Water Bottle</h6>
+            <p class="shop-item-price">$10.00</p>
+        </div>
+        <div class="shop-item">
+            <img class="shop-item-img" src="http://via.placeholder.com/240x290">
+            <h6 class="shop-item-title">Water Bottle</h6>
+            <p class="shop-item-price">$10.00</p>
+        </div>
+      </div>
+    </div>
+  </div> <!-- end of main div -->
+</div> <!-- end of container div -->
+<?php include_once('footer.php'); ?>
