389b1a8bfa57a552e00475ba7e3416bcb583fa63
Landon Call <landon@thecallfamily.com>
2017-10-25 22:12:07 -0600

    Fixed global scss issues with specific id wrapper

--- details.php
+++ details.php
@@ -5,31 +5,31 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<img src="">
-	<form>
-		
-		<h1>Contigo</h1>
-		
+	<div id="details">
+		<img src="">
+		<form>
 
-		<h2>Matterhorn</h2>  
-		
+			<h1>Contigo</h1>
 
-		<h2 class="yo">Size</h2>  
-		<select>
-		  	<option value="">20 oz</option>		  	
-		</select>
 
-		<h2 class="yo">Color</h2>  
-		<select>
-		  	<option value="">Blue</option>
-		</select>
+			<h2>Matterhorn</h2>
 
-		<p>In stock.</p>
-		<div class="description">
-			<p>Cras ultricies ligula sed magna dictum porta. Proin eget tortor risus. Donec rutrum congue leo eget malesuada. Pellentesque in ipsum id orci porta dapibus. Pellentesquuris blandit aliquet elit, eget tincidunt nibh pulvinar a. Donec sollicitudin molestie malesuada.</p>
-		</div>
-	</form>
-	
 
+			<h2 class="yo">Size</h2>
+			<select>
+			  	<option value="">20 oz</option>
+			</select>
+
+			<h2 class="yo">Color</h2>
+			<select>
+			  	<option value="">Blue</option>
+			</select>
+
+			<p>In stock.</p>
+			<div class="description">
+				<p>Cras ultricies ligula sed magna dictum porta. Proin eget tortor risus. Donec rutrum congue leo eget malesuada. Pellentesque in ipsum id orci porta dapibus. Pellentesquuris blandit aliquet elit, eget tincidunt nibh pulvinar a. Donec sollicitudin molestie malesuada.</p>
+			</div>
+		</form>
+	</div>
 </body>
 </html>
