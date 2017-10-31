65ac343b09303268bba50327e21305643b2b56b3
Krista Bice <bubbybice@hotmail.com>
2017-10-30 18:48:54 -0600

    changes

--- card.php
+++ card.php
@@ -1,4 +1,35 @@
+<?php
+session_start();
 
+if(isset($_POST['submitInfo'])){
+$name = $_POST['cardName'];
+$number = $_POST['cardNumber'];
+$ccv = $_POST['cardSecurity'];
+$date = $_POST['cardExpire'];
+$billing = $_POST['billing'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$zip = $_POST['zip'];
+
+	
+	
+
+require_once('variables.php');
+
+//BUILD THE DATABASE CONNECTION WITH host, user, pass, database
+$dbconnection = mysqli_connect(HOST,USER,PASSWORD,DB_NAME) or die ('connection failed');
+
+//BUILD THE query
+$query = "INSERT INTO cc_info( number, cvv, date, name, billing, city, state, zip) VALUES ('$number','$ccv','$date', '$name', '$billing','$city','$state','$zip')";
+
+//NOW TRY AND TALK TO THE database
+$result = mysqli_query($dbconnection, $query) or die ('query failed');
+
+
+	
+	
+}
+?>
 
 <?php include_once('header.php')?>
     <title>Checkout - Card</title>
