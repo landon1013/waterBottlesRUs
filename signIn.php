693623153b1175a6112a32dd8a2306bbaa9cddac
Landon Call <landon@thecallfamily.com>
2017-10-30 21:05:16 -0600

    Change styling issues

--- signIn.php
+++ signIn.php
@@ -2,28 +2,28 @@
 
 require_once('variables.php');
 
-// Try to log the user in 
+// Try to log the user in
 if (isset($_POST['submit'])) {
-	
+
 	//BUILD THE DATABASE CONNECTION WITH host, user, pass, database
 	$dbconnection = mysqli_connect(HOST,USER,PASSWORD,DB_NAME) or die ('connection failed');
-	
+
 	$user = mysqli_real_escape_string($dbconnection, trim($_POST['username']));
 	$pwd = mysqli_real_escape_string($dbconnection, trim($_POST['password']));
-	
+
 	// Get username and password from database
 	$query = "SELECT * FROM usersList WHERE username='$user' AND password=SHA('$pwd')";
 	$data = mysqli_query($dbconnection, $query) or die ('query failed...');
-	
+
 	if (mysqli_num_rows($data) == 1) {
 		$row = mysqli_fetch_array($data);
 		setcookie('username', $row['username'], time() + (60*60*24*30)); // Set to expire in 30 days
 		setcookie('firstname', $row['firstname'], time() + (60*60*24*30)); // Set to expire in 30 days
 		setcookie('lastname', $row['lastname'], time() + (60*60*24*30)); // Set to expire in 30 days
-		
+
 		// Move to the page after loging in
 		header('location: index.php');
-		
+
 	} else {
 		//echo 'Could not found you, try again...';
 		$resultDisplay = 'Could not found you, try again...';
@@ -39,27 +39,27 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="utf-8">
     <link href="scss/reset.css" rel="stylesheet" type="text/css">
-    <link href="scss/main.css" rel="stylesheet" type="text/css">
-    
+    <link href="scss/style.css" rel="stylesheet" type="text/css">
+
    <!-- link font awesome -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-   
+
   <!-- import google font -->
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
   <title>Sing In | Sign Up</title>
-    
+
  </head>
 
   <body>
 
   	<?php include_once('nav.php'); ?>
     <div id="container">
-      
-      
-		
+
+
+
 		<div class="content-title">
 			<div>
-      			<h1>ACCOUNT LOGIN</h1> 
+      			<h1>ACCOUNT LOGIN</h1>
       		</div>
       	</div>
 		<div class="content signin-content">
@@ -71,7 +71,7 @@ if (isset($_POST['submit'])) {
 						<h3>I am a new Customer</h3>
 						<br>
 						<p>By creating an account you will be able to shop faster, be up to date on an orders status, and keep track of the orders you have previously made.</p>
-					
+
 						<div class="submit-container login-btn1-position">
 							<a href="signUp.php">
 								<div class="submit-button continue">Continue</div>
@@ -100,22 +100,21 @@ if (isset($_POST['submit'])) {
 					<br>
 					<div class="result-display">
 
-						<?php 
+						<?php
 							if(isset($resultDisplay)) {
 								echo($resultDisplay);
 							}
 						?>
 					</div>
 		      </section> <!-- End second block -->
-				
+
 		    </div> <!-- End block -->
 
       	</div> <!-- end content -->
-      	
+
     </div> <!-- End container -->
 
 	<?php include_once('footer.php'); ?>
 
   </body>
 </html>
-
